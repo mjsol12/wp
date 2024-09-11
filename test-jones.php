9aebdcca3d6d9e01f54026342025128fc7dfab7c
Jones Solano <mjsolano12345@gmail.com>
2024-09-11 12:02:34 +0800

    test

--- test-jones.php
+++ test-jones.php
@@ -7,7 +7,7 @@
  * Register the "book" custom post type
  */
 function pluginprefix_setup_post_type() {
-	register_post_type( 'book', ['public' => true ] ); 
+	register_post_type( 'mark', ['public' => true ] ); 
 } 
 add_action( 'init', 'pluginprefix_setup_post_type' );
 
@@ -27,7 +27,7 @@ register_activation_hook( __FILE__, 'pluginprefix_activate' );
  */
 function pluginprefix_deactivate() {
 	// Unregister the post type, so the rules are no longer in memory.
-	unregister_post_type( 'book' );
+	unregister_post_type( 'mark' );
 	// Clear the permalinks to remove our post type's rules from the database.
 	flush_rewrite_rules();
 }
