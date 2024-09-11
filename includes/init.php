95158ac375f097dcebd51ece5b450453c8451f76
Jones Solano <mjsolano12345@gmail.com>
2024-09-11 14:58:26 +0800

    sepearation to external directory

---
+++ includes/init.php
@@ -0,0 +1,60 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+// Function to register the custom post type
+function pluginprefix_setup_post_type() {
+    $labels = array(
+        'name'               => _x('My Custom Plugin', 'post type general name'),
+        'singular_name'      => _x('MCP', 'post type singular name'),
+        'menu_name'          => __('MCP'),
+        'name_admin_bar'     => __('MCP'),
+        'add_new'            => __('Add New'),
+        'add_new_item'       => __('Add New MCP'),
+        'edit_item'          => __('Edit MCP'),
+        'new_item'           => __('New MCP'),
+        'view_item'          => __('View MCP'),
+        'all_items'          => __('All MCP'),
+        'search_items'       => __('Search MCP'),
+        'not_found'          => __('No MCPs found'),
+        'not_found_in_trash' => __('No MCPs found in Trash')
+    );
+    
+    $args = array(
+        'labels'             => $labels,
+        'public'             => true,
+        'show_ui'            => true, // ensures that the post type is displayed in the WordPress admin dashboard.
+        'show_in_menu'       => true, // ensures that the custom post type will appear in the admin menu.
+        'show_in_admin_bar'  => true, // Makes the custom post type available in the top admin bar when adding new items.
+        'show_in_menu'       => true,
+        'menu_position'      => 5, // Position of the MCP tab in the menu
+        'supports'           => array('title', 'editor', 'thumbnail', 'excerpt', 'comments', 'revisions'), // Enable the default WordPress editor features
+        'menu_icon'          => 'dashicons-admin-post', // You can change the icon if needed
+		'show_in_rest'       => true,  // Enable REST API for Gutenberg editor
+        'hierarchical'       => false, // Set to false if it's like "posts", true if it's like "pages"
+    );
+
+    register_post_type('mcp', $args);
+}
+add_action( 'init', 'pluginprefix_setup_post_type' );
+
+/**
+ * Activate the plugin.
+ */
+function pluginprefix_activate() { 
+	// Trigger our function that registers the custom post type plugin.
+	pluginprefix_setup_post_type(); 
+	// Clear the permalinks after the post type has been registered.
+	flush_rewrite_rules(); 
+}
+register_activation_hook( __FILE__, 'pluginprefix_activate' );
+
+/**
+ * Deactivation hook.
+ */
+function pluginprefix_deactivate() {
+	// Unregister the post type, so the rules are no longer in memory.
+	unregister_post_type( 'book' );
+	// Clear the permalinks to remove our post type's rules from the database.
+	flush_rewrite_rules();
+}
+register_deactivation_hook( __FILE__, 'pluginprefix_deactivate' );
