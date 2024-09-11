f0cbbc7b6e563bf682102f9dd89e42636df33278
Jones Solano <mjsolano12345@gmail.com>
2024-09-11 11:37:53 +0800

    first init

---
+++ test-jones.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * Plugin Name: jones Testing
+ */
+
+ /**
+ * Register the "book" custom post type
+ */
+function pluginprefix_setup_post_type() {
+	register_post_type( 'book', ['public' => true ] ); 
+} 
+add_action( 'init', 'pluginprefix_setup_post_type' );
+
+/**
+ * Activate the plugin.
+ */
+function pluginprefix_activate() { 
+	// Trigger our function that registers the custom post type plugin.
+	pluginprefix_setup_post_type(); 
+	// Clear the permalinks after the post type has been registered.
+	flush_rewrite_rules(); 
+}
+register_activation_hook( __FILE__, 'pluginprefix_activate' );
+
+/**
+ * Deactivation hook.
+ */
+function pluginprefix_deactivate() {
+	// Unregister the post type, so the rules are no longer in memory.
+	unregister_post_type( 'book' );
+	// Clear the permalinks to remove our post type's rules from the database.
+	flush_rewrite_rules();
+}
+register_deactivation_hook( __FILE__, 'pluginprefix_deactivate' );
